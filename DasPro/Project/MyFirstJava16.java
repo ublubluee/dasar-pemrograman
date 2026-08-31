91506f08bbeb56ab9beeacd87e31dc9a62f39261
ublubluee <hayu.syahlanayla@gmail.com>
2026-08-31 19:17:06 +0700

    Add files via upload

---
+++ DasPro/Project/MyFirstJava16.java
@@ -0,0 +1,9 @@
+public class MyFirstJava16 {
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Syahla");
+
+        System.out.print("\nBelajar Java itu mudah!");
+        System.out.print("\nSemakin sering mencoba, semakin jago.");
+        //System.out.print("\nSemakin sering mencoba, semakin jago.");
+    }
+}
